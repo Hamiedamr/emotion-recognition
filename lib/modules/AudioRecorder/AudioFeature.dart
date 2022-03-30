d52dbda448578aa504fcbe62ce8260f691833a85
AdhamNour <adhamnourelwaffaa@gmail.com>
2022-03-30 21:47:35 +0200

    recording connected

--- lib/modules/AudioRecorder/AudioFeature.dart
+++ lib/modules/AudioRecorder/AudioFeature.dart
@@ -2,6 +2,7 @@ import 'package:authfirebase/modules/AudioRecorder/AudioPlayer.dart';
 import 'package:authfirebase/modules/AudioRecorder/AudioRecorder.dart';
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart' as ap;
+import 'package:http/http.dart' as http;
 
 class AudioFeature extends StatefulWidget {
   const AudioFeature({Key? key}) : super(key: key);
@@ -26,7 +27,12 @@ class _AudioFeatureState extends State<AudioFeature> {
             ),
           )
         : AudioRecorder(
-            onStop: (path) {
+            onStop: (path) async {
+              var request = http.MultipartRequest(
+                  'POST', Uri.parse('http://192.168.1.13/MR/MR/upload.php'));
+              request.files
+                  .add(await http.MultipartFile.fromPath('picture', path));
+              var res = await request.send();
               setState(() {
                 audioSource = ap.AudioSource.uri(Uri.parse(path));
                 showPlayer = true;

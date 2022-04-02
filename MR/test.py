368cfedec95a3cc5c41d8eb52c9b6599b5adaa00
hamiedamr <abdelhamied.amr@outlook.com>
2022-04-03 01:33:28 +0200

    Model

--- MR/test.py
+++ MR/test.py
@@ -1,1 +1,29 @@
-print("hello")
+
+import librosa
+import os
+from tensorflow import keras
+from tensorflow.keras import backend as K
+import numpy as np
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+y, sr = librosa.load(os.path.join("uploads/", "blob.wav"))
+ps =  librosa.feature.melspectrogram (y=y ,  sr=sr )
+
+ps = ps.flatten()[:2376].reshape(1,2376,1)
+emotions = ["Neutral", "Calm", "Happy", "Sad", "Angry", "Fearful", "Disgust", "Surprised"]
+model = keras.models.load_model('./res_model.h5', custom_objects={'f1_m': f1_m})
+print(emotions[np.argmax(model.predict(ps)) - 1])
--- lib/modules/AudioRecorder/AudioFeature.dart
+++ lib/modules/AudioRecorder/AudioFeature.dart
@@ -16,31 +16,31 @@ class _AudioFeatureState extends State<AudioFeature> {
   ap.AudioSource? audioSource;
   @override
   Widget build(BuildContext context) {
-    return showPlayer
-        ? Padding(
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            child: AudioPlayer(
-              source: audioSource!,
-              onDelete: () {
-                setState(() => showPlayer = false);
+    return Column(children: [
+      showPlayer
+          ? Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25),
+              child: AudioPlayer(
+                source: audioSource!,
+                onDelete: () {
+                  setState(() => showPlayer = false);
+                },
+              ),
+            )
+          : AudioRecorder(
+              onStop: (path) async {
+                var request = http.MultipartRequest(
+                    'POST', Uri.parse('http://192.168.1.11/MR/MR/upload.php'));
+                request.files
+                    .add(await http.MultipartFile.fromPath('record', path));
+                var res = await request.send();
+                res.stream.bytesToString().then((value) => print(value));
+                setState(() {
+                  audioSource = ap.AudioSource.uri(Uri.parse(path));
+                  showPlayer = true;
+                });
               },
-            ),
-          )
-        : AudioRecorder(
-            onStop: (path) async {
-              print(path);
-              var request = http.MultipartRequest(
-                  'POST', Uri.parse('http://192.168.1.11/MR/MR/upload.php'));
-              request.files
-                  .add(await http.MultipartFile.fromPath('record', path));
-              print(request.toString());
-              var res = await request.send();
-
-              setState(() {
-                audioSource = ap.AudioSource.uri(Uri.parse(path));
-                showPlayer = true;
-              });
-            },
-          );
+            )
+    ], mainAxisSize: MainAxisSize.min,);
   }
 }
